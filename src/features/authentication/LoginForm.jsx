c30cc1fe544510383bc142c514701995d8d97e54
OmarZahrah <omar.zahrah01@gmail.com>
2024-10-17 10:07:54 +0300

    readme file

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("omar@example.com");
-  const [password, setPassword] = useState("omar1234");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
